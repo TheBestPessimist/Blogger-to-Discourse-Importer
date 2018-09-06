02be6a80c0f32e0b3e30ebed5112c1f6046d04c2
TheBestPessimist <cristian@tbp.land>
2018-09-06 17:46:44 +0200

    Can search and delete category by name (2)
    Forgot to add class

---
+++ src/main/kotlin/tbp/discourse/client/DiscourseCategory.kt
@@ -0,0 +1,12 @@
+package tbp.discourse.client
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+/**
+ * This dataclass is used as a pojo for parsing the /categories.json route with jackson.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class DiscourseCategory(
+    val id: Int,
+    val name: String
+)
